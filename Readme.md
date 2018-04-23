d42aafc8d466fc6017155bb3aa005a8e25c77d89
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Unit Conversion Library code samples
+
+
+<p>This example illustrates how to use the Unit Conversion Library to create a QuantityValue object that represents a pressure value, assign a value measured in millimeters of mercury and gets the same value measured in hectopascals.<br><br>The<strong> Universal Subscription</strong> or an additional <strong>Document Server Subscription</strong> is required to use this example in production code. Please refer to the <a href="http://www.devexpress.com/Subscriptions/">DevExpress Subscription</a> page for pricing information.</p>
+
+<br/>
+
+
---
+++ VB/UnitConversion_Examples/Program.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#usings"
+Imports DevExpress.UnitConversion
+Imports System.Diagnostics
+#End Region ' #usings
+
+Namespace UnitConversion_Examples
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+'			#Region "#metricprefix"
+			' The pressure value is set to 760 mmHg.
+			Dim pressure As QuantityValue(Of Pressure) = (760).MmHg()
+			' Since it is a quantity value, it should be transformed to a proper measurement unit
+			' to obtain a value for display or for comparison.
+			' The pressure is obtained in Pascals and then converted into hectoPascals.
+			Dim prefixConverter As New MetricUnitsConverter()
+			Dim pressure_in_hPa As Double = prefixConverter.Convert(pressure.ToPascals(), MetricPrefix.None, MetricPrefix.Hecto)
+			Debug.WriteLine(pressure_in_hPa)
+'			#End Region ' #metricprefix
+		End Sub
+	End Class
+End Namespace
